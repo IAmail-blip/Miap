44e19f105a5d763c4585e84bec0f95c0366820c0
IAmail-blip <analogicaIA@gmail.com>
2026-06-17 09:37:29 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,53 @@
+// Mi Agenda - Service Worker v4
+var CACHE = 'mi-agenda-v5';
+var FILES = ['./', './index.html'];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(cache) {
+      return cache.addAll(FILES);
+    }).then(function() {
+      return self.skipWaiting();
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k){ return k !== CACHE; })
+            .map(function(k){ return caches.delete(k); })
+      );
+    }).then(function() {
+      return clients.claim();
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(cached) {
+      if (cached) return cached;
+      return fetch(e.request).then(function(response) {
+        var copy = response.clone();
+        caches.open(CACHE).then(function(cache) {
+          cache.put(e.request, copy);
+        });
+        return response;
+      }).catch(function() {
+        return caches.match('./index.html');
+      });
+    })
+  );
+});
+
+self.addEventListener('notificationclick', function(e) {
+  e.notification.close();
+  e.waitUntil(
+    clients.matchAll({type:'window'}).then(function(cs) {
+      if (cs.length) { cs[0].focus(); }
+      else { clients.openWindow('./'); }
+    })
+  );
+});
